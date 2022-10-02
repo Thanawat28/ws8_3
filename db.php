502a4ce14126af1a5c15178b7416201e8f8f1693
Thanawat28 <109870899+Thanawat28@users.noreply.github.com>
2022-10-02 11:00:53 +0700

    Update db.php

--- db.php
+++ db.php
@@ -11,18 +11,15 @@
 <?php
 
 $stmt = $pdo->prepare(""SELECT DISTINCT* FROM member");
-
-
 $stmt->execute();
 
-$x = 1;
 while ($row = $stmt->fetch()) { 
 echo "ชื่อสมาชิก: ".$row["name"]."<br>";
 echo "ที่อยู่: ".$row["address"]."<br>";
 echo "อีเมลล์: ".$row["email"]."<br>";
 echo "<img src = 'imgws3/$x.jpg'"."<br>";
-$x++;
 echo "<br>";
+echo "<hr>";
 }
 ?>
 </body>
